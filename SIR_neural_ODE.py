bug in virus values, broadcasting

SIR_neural_ODE.py
@@ -23,16 +23,16 @@ def generate_SIR_data(model, num_steps):
 
 
 # setting up SIR reference data
-num_hosts = 6
+num_hosts = 10
 num_steps = 500
 dt = 0.01
 torch.manual_seed(0)
 
-time_scale = 1.0  # can make time "move faster" by scaling these constants beyond [0, 1]
-beta = time_scale*0.95  # infection rate
-gamma = time_scale*1.0  # recovery rate
+time_scale = 12.0  # can make time "move faster" by scaling these constants beyond [0, 1]
+beta = time_scale*0.999  # infection rate
+gamma = time_scale*0.1  # recovery rate
 SIR_ODE = SIR(num_hosts, beta, gamma)
-SIR_x0 = np.array([0.5, 0.3, 0.2])
+SIR_x0 = np.array([0.3, 0.5, 0.2])
 
 
 # generate data
@@ -47,8 +47,8 @@ step_size = dt/2.0
 device = 'cpu'  # torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 model = nUIV_NODE(num_hosts, method=method, step_size=step_size).to(device)
 num_epochs = 200
-optimizer = optim.Adam(model.parameters(), lr=5e-2, weight_decay=0.0)
-scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=5, verbose=True)
+optimizer = optim.Adam(model.parameters(), lr=1e-1, weight_decay=0.0)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.25, patience=8, verbose=True)
 loss_function = nn.L1Loss()
 
 for epoch in range(num_epochs):

SIR_sysID.py
@@ -69,11 +69,11 @@ nUIV_stepper = integrator(nUIV_ODE, nUIV_x0, dt)
 nUIV_states = np.zeros((3*num_hosts, num_steps))
 nUIV_states[:, 0] = nUIV_x0
 
-time_scale = 1.0  # can make time "move faster" by scaling these constants beyond [0, 1]
-beta = time_scale*0.95  # infection rate
-gamma = time_scale*1.0  # recovery rate
+time_scale = 12.0  # can make time "move faster" by scaling these constants beyond [0, 1]
+beta = time_scale*0.999  # infection rate
+gamma = time_scale*0.1  # recovery rate
 SIR_ODE = SIR(num_hosts, beta, gamma)
-SIR_x0 = np.array([0.5, 0.3, 0.2])
+SIR_x0 = np.array([0.3, 0.5, 0.2])
 
 
 SIR_stepper = integrator(SIR_ODE, SIR_x0, dt)

odes/neural_ODE.py
@@ -48,23 +48,17 @@ class nUIV_rhs(nn.Module):
 
             rhs[3*n] = - beta * U * V  # U dynamics
             rhs[3*n + 1] = beta * U * V - delta * I  # I dynamics
-            rhs[3*n + 2] = p*I - c*V - self.parametrization(self.ts[n, n])  # V dynamics
-
-            # add transmission between neighbors (including self)
-            # normalize so that total intake is bounded by
-            for nbr in range(n):
-                rhs[3*n + 2] += normalization[nbr]*self.parametrization(self.ts[nbr, n])*state[3*nbr+2]
-            for nbr in range(n+1, self.N):
-                rhs[3*n + 2] += normalization[nbr]*self.parametrization(self.ts[nbr, n])*state[3*nbr+2]
-
+            rhs[3*n + 2] = p*I - c*V - normalization[n]*self.parametrization(self.ts[n, n])*V  # V dynamics
+            rhs[3*n + 2] -= normalization[n]*self.parametrization(self.ts[n, n])*V
+            rhs[3*n + 2] += torch.dot(self.parametrization(self.ts[:, n]) * normalization, state[2::3])
         return rhs
 
     def compute_normalization(self):
         normalization = torch.zeros_like(self.betas)
-        for n in range(self.N):
-            normalization[n] = torch.min(self.parametrization(self.ts[n, n]) /
-                                         (torch.sum(self.parametrization(self.ts[n, :]))
-                                          - self.parametrization(self.ts[n, n])), torch.tensor(1.0))
+        normalization = torch.min(self.parametrization(torch.diag(self.ts)) /
+                                  (torch.sum(self.parametrization(self.ts), axis=1)
+                                   - torch.diag(self.parametrization(self.ts))),
+                                  torch.tensor(1.0))
         return normalization
 
     def normalize_ts(self):